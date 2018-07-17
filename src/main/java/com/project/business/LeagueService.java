26bf66b5d6e56d560c9c9a3dd3d2ae78febbd12c
DavidMartin <DavidMartin@hotmail.com>
2018-07-17 23:39:33 +0100

    refactoring

--- src/main/java/com/project/business/LeagueService.java
+++ src/main/java/com/project/business/LeagueService.java
@@ -14,12 +14,11 @@ public class LeagueService {
 
 	public List<LadderTableEntry> getLeagueDetails(String league) {
 		
+		List<LadderTableEntry> tableEntries = new ArrayList<>();
 		String url = "http://api.pathofexile.com/ladders/"+league+"?offset=1&limit=10";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<MyPojo> response = restTemplate.getForEntity(url, MyPojo.class);
-
-		List<LadderTableEntry> tableEntries = new ArrayList<>();
-
+		
 		for (Entries anEntry : response.getBody().getEntries()) {
 			LadderTableEntry entry = new LadderTableEntry();
 			entry.setName(anEntry.getCharacter().getName());
@@ -27,10 +26,6 @@ public class LeagueService {
 			entry.setChallenges(anEntry.getAccount().getChallenges().getTotal());
 			entry.setExperience(anEntry.getCharacter().getExperience());
 			tableEntries.add(entry);
-		}
-
-		for (LadderTableEntry anEntry : tableEntries) {
-			System.out.println(anEntry);
 		}
 		
 		return tableEntries;
--- src/main/java/com/project/business/Main.java
+++
@@ -1,9 +0,0 @@
-package com.project.business;
-
-public class Main {
-
-	public static void main(String[] args) {
-		LeagueService webController = new LeagueService();
-		webController.getLeagueDetails("SSF Incursion HC");
-	}
-}
